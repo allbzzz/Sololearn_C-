606c6c9b2be8b8383868c56ee94cefefbcf6abf1
allbzzz <146387808+allbzzz@users.noreply.github.com>
2024-03-14 00:41:24 +0300

    Add files via upload

---
+++ Your first C# program.cs
@@ -0,0 +1,24 @@
+/*Your first C# program
+Write a program to print "C# is cool".
+Note that the sentence starts with a capital letter.
+
+Hint
+Both Console.Write() or Console.WriteLine() methods can be used.
+
+Remember to enclose the text into double quotes and end the statement with a semicolon (;).
+*/
+
+
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("C# is cool");
+            
+        }
+    }
+}
