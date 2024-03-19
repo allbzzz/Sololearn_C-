41a9a48b49a9d094580c97beaca25c34c1314cc7
allbzzz <146387808+allbzzz@users.noreply.github.com>
2024-03-19 11:24:02 +0300

    Add files via upload

---
+++ Comments.cs
@@ -0,0 +1,30 @@
+/*Comments
+You are giving an important speech about programming languages.
+You need to write a program to output "C# is the best" during your speech.
+In the code you are given, you have commented lines.
+
+Task
+Uncomment the required line to output the expected sentence.
+
+Simply delete the slashes (//) before the necessary line to uncomment it.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Unnecessary line");
+            Console.WriteLine("C# is the best");
+        }
+    }
+}
